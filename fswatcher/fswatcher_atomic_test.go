3eb33dcadafbdd71c740b53d4108cbbe192ab490
Terin Stock <terinjokes@gmail.com>
2021-08-01 22:37:11 -0700

    chore: document the atomic writer pattern
    When certinels reload certificates from disk, either via timer or being
    notified by the filesystem, they reload both the certificates and the
    keys from disk. If the certificate and keys do not match, the certinels
    exit with an error. This can cause problems if the keys and certificates
    are not updated together atomically, as the watcher may trigger after
    only one has been updated.
    
    This changeset adds a test for Kubernetes's atomic writer[1] pattern,
    and documents a similar pattern that users who update key and
    certificates can follow.
    
    [1]: https://github.com/kubernetes/kubernetes/blob/v1.21.3/pkg/volume/util/atomic_writer.go#L49-L54
    
    Fixes #14

---
+++ fswatcher/fswatcher_atomic_test.go
@@ -0,0 +1,91 @@
+// +build !windows
+
+package fswatcher
+
+import (
+	"context"
+	"crypto/x509"
+	"math/big"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/cloudflare/certinel/internel/pkitest"
+	"github.com/google/go-cmp/cmp"
+	"golang.org/x/sync/errgroup"
+	"gotest.tools/v3/assert"
+	is "gotest.tools/v3/assert/cmp"
+	"gotest.tools/v3/fs"
+	"gotest.tools/v3/poll"
+)
+
+const (
+	atomicDirName    = "..data"
+	atomicNewDirName = "..data_tmp"
+)
+
+func TestWatcher_AtomicWriter(t *testing.T) {
+	t.Parallel()
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	pk1 := pkitest.NewPrivateKey(t)
+	pemPK1 := pkitest.EncodePrivateKey(t, pk1)
+
+	pk2 := pkitest.NewPrivateKey(t)
+	pemPK2 := pkitest.EncodePrivateKey(t, pk2)
+
+	dir := fs.NewDir(t, "test-symlink",
+		fs.WithDir(".first",
+			fs.WithFile("my.crt", "", fs.WithBytes(
+				pkitest.PemEncodeCertificate(t, x509.Certificate{
+					SerialNumber: big.NewInt(1),
+				}, pk1),
+			)),
+			fs.WithFile("my.key", "", fs.WithBytes(pemPK1)),
+		),
+		fs.WithDir(".second",
+			fs.WithFile("my.crt", "", fs.WithBytes(
+				pkitest.PemEncodeCertificate(t, x509.Certificate{
+					SerialNumber: big.NewInt(2),
+				}, pk2),
+			)),
+			fs.WithFile("my.key", "", fs.WithBytes(pemPK2)),
+		),
+		fs.WithSymlink(atomicDirName, ".first"),
+		fs.WithSymlink("my.key", filepath.Join(atomicDirName, "my.key")),
+		fs.WithSymlink("my.crt", filepath.Join(atomicDirName, "my.crt")),
+	)
+	defer dir.Remove()
+
+	watcher, err := New(dir.Join("my.crt"), dir.Join("my.key"))
+	assert.NilError(t, err)
+
+	cert, err := watcher.GetCertificate(nil)
+	assert.NilError(t, err)
+	assert.DeepEqual(t, cert.Leaf.SerialNumber, big.NewInt(1), cmp.Comparer(pkitest.CmpBigInt))
+
+	g, gctx := errgroup.WithContext(ctx)
+	g.Go(func() error {
+		return watcher.Start(gctx)
+	})
+	<-time.After(100 * time.Millisecond)
+
+	fs.Apply(t, dir, fs.WithSymlink(atomicNewDirName, ".second"))
+
+	err = os.Rename(dir.Join(atomicNewDirName), dir.Join(atomicDirName))
+	assert.NilError(t, err)
+
+	poll.WaitOn(t, func(t poll.LogT) poll.Result {
+		cert, err := watcher.GetCertificate(nil)
+		if err != nil {
+			return poll.Error(err)
+		}
+
+		return poll.Compare(is.DeepEqual(cert.Leaf.SerialNumber, big.NewInt(2), cmp.Comparer(pkitest.CmpBigInt)))
+	})
+
+	cancel()
+	assert.ErrorType(t, g.Wait(), context.Canceled)
+}
